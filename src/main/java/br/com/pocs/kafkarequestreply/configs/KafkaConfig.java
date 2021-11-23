550ef01937e76cc248966fd0cdaa13672ffe96e9
Lincoln Araujo <lincoln.n.araujo@gmail.com>
2021-11-23 09:52:31 -0300

    fix: configs kafka

--- src/main/java/br/com/pocs/kafkarequestreply/configs/KafkaConfig.java
+++ src/main/java/br/com/pocs/kafkarequestreply/configs/KafkaConfig.java
@@ -1,17 +1,27 @@
 package br.com.pocs.kafkarequestreply.configs;
 
-import br.com.pocs.kafkarequestreply.dto.PagamentoResponse;
 import br.com.pocs.kafkarequestreply.dto.PagamentoDto;
+import br.com.pocs.kafkarequestreply.dto.PagamentoResponse;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Value("${kafka.group.id}")
@@ -19,6 +29,9 @@ public class KafkaConfig {
 
     @Value("${kafka.reply.topic}")
     private String replyTopic;
+
+    @Value("${kafka.bootstrap.server}")
+    private String bootStrapServer;
 
     @Bean
     public ReplyingKafkaTemplate<String, PagamentoDto, PagamentoResponse> replyingKafkaTemplate(ProducerFactory<String, PagamentoDto> pf,
@@ -36,5 +49,54 @@ public class KafkaConfig {
         factory.setReplyTemplate(kafkaTemplate);
         return kafkaTemplate;
     }
+
+
+
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        return props;
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, PagamentoResponse> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), new JsonDeserializer<>(PagamentoResponse.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PagamentoResponse> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PagamentoResponse> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setReplyTemplate(replyTemplate());
+        return factory;
+    }
+
+    @Bean
+    public ProducerFactory<String, PagamentoDto> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, PagamentoDto> replyTemplate() {
+        KafkaTemplate<String, PagamentoDto> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setDefaultTopic(replyTopic);
+        return kafkaTemplate;
+    }
+
 
 }
--- src/main/java/br/com/pocs/kafkarequestreply/kafka/ValidacaoPagamentoListener.java
+++ src/main/java/br/com/pocs/kafkarequestreply/kafka/ValidacaoPagamentoListener.java
@@ -1,8 +1,11 @@
 package br.com.pocs.kafkarequestreply.kafka;
 
-import br.com.pocs.kafkarequestreply.dto.PagamentoResponse;
 import br.com.pocs.kafkarequestreply.dto.PagamentoDto;
+import br.com.pocs.kafkarequestreply.dto.PagamentoResponse;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +14,16 @@ public class ValidacaoPagamentoListener {
 
     @KafkaListener(topics = "${kafka.request.topic}", groupId = "${kafka.group.id}")
     @SendTo
-    public PagamentoResponse handle(PagamentoDto dto) throws InterruptedException {
+    public PagamentoResponse handle(ConsumerRecord<String, PagamentoDto> responseConsumerRecord, @Header(KafkaHeaders.CORRELATION_ID) byte[] correlation) throws InterruptedException {
 
-        System.out.println("Validando Pagamento: "+ dto);
+        responseConsumerRecord.headers().add(KafkaHeaders.CORRELATION_ID, correlation);
+
+        System.out.println("Validando Pagamento: "+ responseConsumerRecord);
         Thread.sleep(7000);
 
         PagamentoResponse response = new PagamentoResponse();
-        response.setId(dto.getId());
-        response.setDataPagamento(dto.getDataPagamento());
+        response.setId(responseConsumerRecord.value().getId());
+        response.setDataPagamento(responseConsumerRecord.value().getDataPagamento());
         response.setSituacaoPagamento("pagamento_validado");
 
         System.out.println("Pagamento validado: " + response);
